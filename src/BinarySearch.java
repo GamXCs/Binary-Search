1411e88e22bcd8eed88a7d358134f6ddffe90f70
Gam Stewart <gavlevi@uab.com>
2024-12-28 02:56:59 -0600

    Added tester to make sure binary algo worked

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,5 +1,8 @@
+import java.util.Arrays;
+
 public class BinarySearch {
-    public int binarySearch(int[] array, int target){
+
+    public static int binarySearch(int[] array, int target){
         int low = 0;
         int high = array.length - 1; //array starts at 0, hence the -1
 
@@ -14,5 +17,14 @@ public class BinarySearch {
             }
         }
         return -1;
+    }
+    public static void main(String[] args) {
+        int[] array = {1,58,9,87,52,6,5,15};
+        Arrays.sort(array);
+
+        System.out.println(Arrays.toString(array));//Test to make sure array was sorted
+
+        int target = 87;
+        System.out.println("Target value is found at index: " + binarySearch(array,target) );
     }
 }
